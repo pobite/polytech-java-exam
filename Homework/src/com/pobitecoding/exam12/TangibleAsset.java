735a1fccbaa452feb9733e495f32d33d9fe02c09
unknown <ehdgkr5986@naver.com>
2023-03-15 17:37:21 +0900

    Add exam12-4

--- Homework/src/com/pobitecoding/exam12/TangibleAsset.java
+++ Homework/src/com/pobitecoding/exam12/TangibleAsset.java
@@ -1,5 +1,16 @@
 package com.pobitecoding.exam12;
 
-public abstract class TangibleAsset extends Asset {
+public abstract class TangibleAsset extends Asset implements Thing{
     
+    private double weight;
+    
+    @Override
+    public double getWeight() {
+        return weight;
+    }
+    
+    @Override
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
 }
