5cc1158c02d09d7393a2a6e7bbe4b7c10f3182b1
unknown <ehdgkr5986@naver.com>
2023-03-16 16:31:30 +0900

    ADD EXAM 13-2

--- Homework/src/com/pobitecoding/exam13/Main.java
+++ Homework/src/com/pobitecoding/exam13/Main.java
@@ -2,8 +2,28 @@ package com.pobitecoding.exam13;
 
 public class Main {
     
-    X ojb = new A();
-    
-    
-    
+    public static void main(String[] args) {
+        // 1. X obj = new A(); 로 A인스턴스를 생성한 후,
+        // 변수 obj에서 호출할 수 있는 메소드를 a(), b(), c() 중에 골라보시오
+        
+        X obj = new A();
+        
+        // 답: a()
+
+        
+        // 2. Y y1 = new A();
+        // Y y2 = new B(); 로 A와 B의 인스턴스를 생성한 후 
+        // y1.a();
+        // y2.a(); 를 실행했을 때에 화면에 표시되는 내용을 말하시오.
+        
+        Y y1 = new A();
+        Y y2 = new B();
+        
+        y1.a();
+        y2.a();
+        
+        // 답
+        // Aa
+        // Ba
+    }
 }
