d8d7144201c161cd6e3dc33daf2d7218b0e41ce0
unknown <ehdgkr5986@naver.com>
2023-03-17 14:01:41 +0900

    String 알고리즘(자음 판별)

--- JavaExam/src/com/pobitecoding/test/Word2.java
+++ JavaExam/src/com/pobitecoding/test/Word2.java
@@ -12,8 +12,21 @@ public class Word2 {
 //        return "aeiou".contains(..letters.substring(i, i + 1).toLowerCase());
 //    }
     
-    // 최고의 코드
+    // 모음 최고의 코드
     public boolean isVowel(int i) {
         return "aeiouAEIOU".contains(letters.substring(i, i + 1));
+    }
+    
+//    public boolean isConsonant(int i) {
+//        return !"aeiouAEIOU".contains(letters.substring(i, i + 1));
+//    }
+    
+    // 자음 최고의 코드
+    public boolean isConsonant(int i) {
+        return !isVowel(i);
+    }
+    
+    public boolean isConsonant2(int i) {
+        return letters.substring(i, i + 1).matches("^[aeiouAEIOU]");
     }
 }
