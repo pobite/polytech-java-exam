b5f3e2fff86e317ca049e8a0644e897b0381481b
unknown <ehdgkr5986@naver.com>
2023-03-20 16:16:32 +0900

    Add Exam15-3

---
+++ Homework/src/com/pobitecoding/exam15/IntegerExam.java
@@ -0,0 +1,18 @@
+package com.pobitecoding.exam15;
+
+public class IntegerExam {
+    
+    public static void main(String[] args) {
+        
+        // Integer.parseInt() 메소드를 사용하여,
+        // 문자열 “Three” 의 변환결과를 int형 변수 i 에 대입하는 코드를 작성하시오. 
+        // int i = Integer.parseInt("Three");
+        
+        try {
+            int i = Integer.parseInt("Three");
+        }
+        catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+    }
+}
