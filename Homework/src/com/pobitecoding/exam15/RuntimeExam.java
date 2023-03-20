c21ae5c03c6f6149653b9efe16692cbbde2f6829
unknown <ehdgkr5986@naver.com>
2023-03-20 16:08:01 +0900

    Fix Exam15-2

--- Homework/src/com/pobitecoding/exam15/RuntimeExam.java
+++ Homework/src/com/pobitecoding/exam15/RuntimeExam.java
@@ -4,6 +4,15 @@ public class RuntimeExam {
     
     public static void main(String[] args) {
         String s = null;
-        s.length();
+        
+        try {
+            s.length(); 
+        }
+        catch (NullPointerException e) {
+            System.out.println("NullPointerException 예외를 catch 하였습니다");
+            System.out.println("---- stack trace (여기부터) ----");
+            e.printStackTrace();
+            System.out.println("---- stack trace (여기까지) ----");
+        }
     }
 }
