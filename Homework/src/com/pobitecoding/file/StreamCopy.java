9a5e05df34e6da3c17a72aaac8c5f095a86c0d3f
unknown <ehdgkr5986@naver.com>
2023-03-20 17:45:00 +0900

    Add exam6-2

--- Homework/src/com/pobitecoding/file/StreamCopy.java
+++ Homework/src/com/pobitecoding/file/StreamCopy.java
@@ -1,8 +1,12 @@
 package com.pobitecoding.file;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.zip.GZIPOutputStream;
 
 public class StreamCopy {
     
@@ -59,16 +63,48 @@ public class StreamCopy {
             e.printStackTrace();
         }
         finally {
-            
             try {
                 output.close();
                 input.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
+           
         }
     }
     
+    public static void compress(String copyName, String compressName) {
+        
+        BufferedReader br = null;
+        BufferedOutputStream bos = null;
+        
+        try {
+            // BufferedOutputStream -> GZIPOutStream -> FileOutputStream으로 흐름
+            // 데이터가 흘러간다는 느낌을 받아야 함
+            
+            br = new BufferedReader(new FileReader(copyName));
+            bos = new BufferedOutputStream(
+                    new GZIPOutputStream(new FileOutputStream(compressName)));
+            
+            String s;
+            while (null != (s = br.readLine())) {
+                bos.write(s.getBytes());
+                bos.write("\n".getBytes());
+            }
+            
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                br.close();
+                bos.close();
+                System.out.println(compressName + " 파일의 압축이 완료됐습니다.");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
     
     public static void main(String[] args) {
         
@@ -86,5 +122,9 @@ public class StreamCopy {
         
         // 파일 출력
         print(copyName);
+        
+        // 파일 압축
+        String compressName = "dataCopy.zip";
+        compress(copyName, compressName);
     }
 }
