9cc325b587840e838e6f1a612bb9a853dc4cd3f5
unknown <ehdgkr5986@naver.com>
2023-03-20 16:20:15 +0900

    Add Exam15-4

---
+++ Homework/src/com/pobitecoding/exam15/IOExceptionExam.java
@@ -0,0 +1,14 @@
+package com.pobitecoding.exam15;
+
+import java.io.FileWriter;
+
+public class IOExceptionExam {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("프로그램 시작");
+        FileWriter fw = new FileWriter("data.txt");
+        
+    }
+    
+}
