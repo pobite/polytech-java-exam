8299e43c04d025043a68c36940ba7c1c18e65d2f
unknown <ehdgkr5986@naver.com>
2023-03-21 15:45:59 +0900

    Add exam 8-1 URL icon 저장

---
+++ Homework/src/com/pobitecoding/network/ReadIcon.java
@@ -0,0 +1,49 @@
+package com.pobitecoding.network;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class ReadIcon {
+    
+    public static void main(String[] args) {
+        
+        URL url = null;
+        InputStream is = null;
+        InputStreamReader isr = null;
+        BufferedOutputStream bos = null;
+        
+        try {
+            
+            // output
+            bos = new BufferedOutputStream(new FileOutputStream("icon.ico"));
+          
+            // input
+            url = new URL("https://alimipro.com/favicon.ico");
+            is = url.openStream();
+            isr = new InputStreamReader(is);
+            
+            int i = isr.read();
+            while (i != -1) {
+                bos.write(i);
+                i = isr.read();
+            }
+            
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                isr.close();
+                bos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
