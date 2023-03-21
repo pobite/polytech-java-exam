f2b5ff12b95581d351cae4bb1b990a6d51ce1d88
unknown <ehdgkr5986@naver.com>
2023-03-21 11:11:10 +0900

    람다식 연습

--- JavaExam/src/com/pobitecoding/test/Main3.java
+++ JavaExam/src/com/pobitecoding/test/Main3.java
@@ -3,6 +3,7 @@ package com.pobitecoding.test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main3 {
     
@@ -24,5 +25,80 @@ public class Main3 {
         // 더 줄이기
         nums.stream().forEach(System.out::println);
         
+        // 필터
+        // 필터는 if문 대신 쓴다
+        // 해당 조건으로 거름
+        // 모든 데이터가 줄 서서 이동
+        
+        nums.stream()
+            .filter((num) -> num % 2 == 0)              // 짝수
+            .forEach(System.out::println);
+
+        // 같은 방법
+        for (int i = 0; i < nums.size(); i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+        
+        // 추가 작업이 있을 경우
+        nums.stream()
+        .filter((num) -> num % 2 == 0)              // 짝수
+        .forEach((num) -> System.out.println(num + 1));
+        
+        // 필터 - 거르기
+        // forEach - 무지성 뺑뺑이
+        
+        // map - 변환
+        nums.stream()
+            .filter((num) -> num % 2 == 0)                   // 거르기   2, 4, 6
+            .map((num) -> num + "번")                         // 변환    "2번", "4번", "6번"
+            .forEach((num) -> System.out.println(num + 1));  // 무지성 뺑뺑이
+        
+        // -> 리스트의 타입이 계속 변환한다
+        // 이 세 개는 리턴 타입이 스트림이다
+        // Stream은 void
+        // 그래서 스트림은 마지막에만 올 수 있음
+        // 점찍고 점찍으면서 반복하는게 메서드 체이닝
+        
+        
+        // 기존에 있는 리스트에서 원하는 애만 뽑아서 리스트만 다시 반환
+        
+        List<String> names = new ArrayList<>();
+        names.add("박경덕");
+        names.add("박준하");
+        names.add("이동학");
+        names.add("박태현");
+        
+//        List<String> parks = getParkList(names);
+//        System.out.println("parks");
+        // 출력 - parks
+
+        
+        // 람다식과 스트림 활용
+        
+        System.out.println(names.stream().filter(name -> name.startsWith("박"))
+                                         .collect(Collectors.toList()));
+        // [박경덕, 박준하, 박태현]
     }
+    
+    // 기존 로직
+    public static List<String> getParkList(List<String> names) {
+        // filter가 스트림을 리턴, 리스트로 바꿔야 함
+        return names.stream().filter(name -> name.startsWith("박"))
+                             .collect(Collectors.toList());
+        
+//        List<String> results = new ArrayList<>();
+//        
+//        for (String name: names) {
+//            if (name.startsWith("박")) {
+//                results.add(name);
+//            }
+//        }
+//        return results;
+    }
+    
+    
+    
+    
 }
