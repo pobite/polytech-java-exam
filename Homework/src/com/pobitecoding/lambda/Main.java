224e82cf2edc046eaae50294878cd14c022d3791
unknown <ehdgkr5986@naver.com>
2023-03-21 15:16:07 +0900

    Add exam5-2 람다식

--- Homework/src/com/pobitecoding/lambda/Main.java
+++ Homework/src/com/pobitecoding/lambda/Main.java
@@ -4,7 +4,17 @@ public class Main {
     
     public static void main(String[] args) {
         
+        // 인터페이스에 함수 대입
         Func1 call = Utils::isOdd;
         Func2 call2 = Utils::addNamePrefix;
+        
+        // 람다에 함수 대입
+        call = n -> n % 2 == 1;
+        call2 = (gender, name) -> {
+            if (gender == true) {
+                return "Mr." + name;
+            }
+            return "Ms." + name;
+        };
     }
 }
