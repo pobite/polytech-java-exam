b8d0b95c47e9eae18bfffde520e876605d7291b7
unknown <ehdgkr5986@naver.com>
2023-03-21 16:47:53 +0900

    Fix Unit Test exam1

---
+++ Homework/src/test/com/pobitecoding/unit/CounterTest.java
@@ -0,0 +1,15 @@
+package test.com.pobitecoding.unit;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import com.pobitecoding.unit.Counter;
+
+public class CounterTest {
+
+    @Test
+    public void test() {
+        Counter counter = new Counter();
+        
+    }
+
+}
---
+++ Homework/src/test/com/pobitecoding/unit/DownCounterTest.java
@@ -0,0 +1,13 @@
+package test.com.pobitecoding.unit;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class DownCounterTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
