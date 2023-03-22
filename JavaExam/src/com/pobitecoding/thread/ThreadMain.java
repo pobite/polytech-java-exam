25712281c4d9da400be686f0fa9b2ce53e47ba19
unknown <ehdgkr5986@naver.com>
2023-03-22 11:19:53 +0900

    Main 스레드 2개 확인

--- JavaExam/src/com/pobitecoding/thread/ThreadMain.java
+++ JavaExam/src/com/pobitecoding/thread/ThreadMain.java
@@ -28,34 +28,18 @@ class PrintingThread2 implements Runnable {
 
 public class ThreadMain {
     
-    // 다양한 형태로 사용 가능!
     public static void main(String[] args) {
         
         System.out.println("키 입력");
         Scanner scanner = new Scanner(System.in);
         
-//        new PrintingThread().start();
-
-        // 익명 클래스
-        // ctrl 1을 누르면 Convert to 람다식
-        // 익명 클래스에서 커서 놓고 ctrl1로 편하게 변환 가능
-        new Thread(new Runnable() {
-            
-            @Override
-            public void run() {
-                
-            }
-        }).start();
         
-        //람다식
-        // 람다에서도 ctrl 1로 익명 클래스로 변환 가능
+        // 스레드 2개 있음
+        // 기본 Main에서 동작하는 스레드
         new Thread(() -> {
             System.out.println("ggg");
         }).start();
         
-        // -> ctrl 1로 상호 변환 가능
-        
-        // 인터페이스 타입을 만는 경우 옵션이 많아짐 
         scanner.nextLine();
     }
 }
