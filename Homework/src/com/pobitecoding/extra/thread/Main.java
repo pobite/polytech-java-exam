327f096de7374ae8416323050f4c1e882fc1a890
unknown <ehdgkr5986@naver.com>
2023-03-23 09:31:53 +0900

    Fix exam10-1 thread

--- Homework/src/com/pobitecoding/extra/thread/Main.java
+++ Homework/src/com/pobitecoding/extra/thread/Main.java
@@ -2,13 +2,8 @@ package com.pobitecoding.extra.thread;
 
 public class Main {
 
-    public class CountUpThread extends Thread {
-        
-        @Override
-        public void run() {
-            for (int i = 0; i <= 50; i++) {
-                System.out.println(i);
-            }
-        }
-    }
+    public static void main(String[] args) {
+        new CountUpThread().start();
+        new CountUpThread().start();
+        new CountUpThread().start();
 }
