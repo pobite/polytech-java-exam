b315b21f4d08d4500c7c5f1944786f4d1c81c8f8
unknown <ehdgkr5986@naver.com>
2023-03-23 09:33:20 +0900

    Fix exam10-2 thread

--- Homework/src/com/pobitecoding/extra/thread/Main.java
+++ Homework/src/com/pobitecoding/extra/thread/Main.java
@@ -2,9 +2,18 @@ package com.pobitecoding.extra.thread;
 
 public class Main {
 
-    public static void main(String[] args) {
-        new CountUpThread().start();
-        new CountUpThread().start();
-        new CountUpThread().start();
+    public static void main(String[] args) throws InterruptedException {
+        
+        Counter counter = new Counter();
+        
+        for (int i = 0; i < 50; i++) {
+            new Thread(() -> {
+                counter.add(1);
+                                counter.add(1);
+                                counter.mul(1);
+            }).start();
+        }
+        Thread.sleep(2000);
+        System.out.println("The result is  " + counter.getCount());
     }
 }
