4820082cf31374452ce8445b9a6f1c8e7dab991e
unknown <ehdgkr5986@naver.com>
2023-04-06 19:10:43 +0900

    Fix 주석 추가

--- ConnectionJDBC/src/com/polytech/data/jdbc/JDBC.java
+++ ConnectionJDBC/src/com/polytech/data/jdbc/JDBC.java
@@ -6,10 +6,12 @@ import java.sql.SQLException;
 
 public class JDBC {
     
-    
     public static void main(String[] args) {
+        
+        // 1. Oracle JDBC 드라이버를 로드합니다. 
         Connection conn = null;
         
+        // 2. 데이터베이스에 연결합니다.
         try {
             Class.forName("oracle.jdbc.driver.OracleDriver");
             String url = "jdbc:oracle:thin:@192.168.119.119:1521/dink";
@@ -17,15 +19,20 @@ public class JDBC {
             String passwd = "tiger";
             conn = DriverManager.getConnection(url, user, passwd);
             System.out.println(conn);
-            
+        
         } catch (ClassNotFoundException e) {
-            // TODO Auto-generated catch block
+            // 드라이버 로드 중 예외가 발생한 경우 처리합니다.
             e.printStackTrace();
         } catch (SQLException e) {
-            // TODO Auto-generated catch block
+         // 데이터베이스 연결 및 쿼리 실행 중 예외가 발생한 경우 처리합니다.
             e.printStackTrace();
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                // 연결 닫기 중 예외가 발생한 경우 처리합니다.
+                e.printStackTrace();
+            }
         }
-
     }
-    
 }
